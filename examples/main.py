bc049e64ce057a7e91190601798d900220deb545
Mykyta <mykytakasianenko@gmail.com>
2026-06-10 19:22:40 +0200

    ref

--- examples/main.py
+++ examples/main.py
@@ -5,12 +5,15 @@
 from fastapi import FastAPI
 
 from openadmin.fastapi import AdminPanel
+from openadmin.sqlalchemy import SQLAlchemyPanelPlugin
 
 from . import users
 from .lifespan import lifespan
 
 app = FastAPI(lifespan=lifespan)
-admin_panel = AdminPanel()
+admin_panel = AdminPanel(
+    plugins=[SQLAlchemyPanelPlugin()],
+)
 
 admin_panel.include_page(users.page, tags=["Users"])
 
--- openadmin/sqlalchemy/__init__.py
+++ openadmin/sqlalchemy/__init__.py
@@ -3,7 +3,9 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 from .page_plugin import SQLAlchemyPagePlugin
+from .panel_plugin import SQLAlchemyPanelPlugin
 
 __all__ = [
     "SQLAlchemyPagePlugin",
+    "SQLAlchemyPanelPlugin",
 ]
