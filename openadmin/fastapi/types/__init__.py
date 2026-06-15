f0d7f979720305df23c26ab65f53cc4c506b132b
Mykyta <mykytakasianenko@gmail.com>
2026-06-15 21:54:42 +0200

    ref

---
+++ openadmin/fastapi/types/__init__.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2026 OpenAdmin
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from .page_protocol import PageProtocol
+from .section import Section
+from .stat import Stat
+from .table import Table
+
+__all__ = [
+    "PageProtocol",
+    "Section",
+    "Stat",
+    "Table",
+]
---
+++ openadmin/fastapi/types/table.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2026 OpenAdmin
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from pydantic import BaseModel
+
+from openadmin import spec
+
+
+class Table(BaseModel):
+    function_name: str
+    method: spec.HttpMethod
+    name: str
+    description: str | None
