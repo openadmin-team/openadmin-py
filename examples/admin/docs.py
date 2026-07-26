bb44728f0f6635ba1d26fb26520c28530e2bbfc6
Mykyta <mykytakasianenko@gmail.com>
2026-07-26 10:46:39 +0200

    ref

--- examples/admin/docs.py
+++ examples/admin/docs.py
@@ -2,13 +2,13 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-from openadmin import fastapi
+from openadmin import fastapi, spec
 
 page = fastapi.AdminPage("Docs", icon="dock")
 
 
-@page.markdown("Comany policy", icon="podcast", color="blue")
-def company_policy():
+@page.markdown("Comany policy 1", icon="podcast", color="blue")
+def company_policy_1() -> spec.Markdown:
     return """
 # Hello World
 
@@ -19,3 +19,21 @@ This is new text
 - Cake
 - Limonade
 """
+
+
+@page.markdown("Comany policy 2")
+def company_policy_2() -> spec.Markdown:
+    return {
+        "content": """
+                    # Hello World
+
+                    This is new text
+
+                    ---
+
+                    - Cake
+                    - Limonade
+        """,
+        "color": "yellow",
+        "icon": "a-arrow-up",
+    }
