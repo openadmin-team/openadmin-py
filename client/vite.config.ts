46828a82225f8cab783910ade48b73f8289a5c08
Mykyta <mykytakasianenko@gmail.com>
2026-08-09 13:42:28 +0200

    ref

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: './',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
