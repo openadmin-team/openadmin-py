d0f27e543c2f4f85e30582d35d1ccdd7c2eccf54
Mykyta <mykytakasianenko@gmail.com>
2026-08-09 16:38:34 +0200

    ref

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
+import { useOpenAdminSpec } from '@/composables/openadmin-spec';
+
+const { data, isLoading } = useOpenAdminSpec()
 </script>
 
 <template>
 	<h1>Hello</h1>
+	{{ isLoading }}
+	{{ data }}
 </template>
