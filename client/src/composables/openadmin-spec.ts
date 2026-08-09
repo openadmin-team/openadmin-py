3dd6fb0334d04a5077536cf5f9474cf512379fce
Mykyta <mykytakasianenko@gmail.com>
2026-08-09 17:03:43 +0200

    ref

--- client/src/composables/openadmin-spec.ts
+++ client/src/composables/openadmin-spec.ts
@@ -1,16 +1,22 @@
 import { useQuery } from "@tanstack/vue-query"
+import type { Error as ApiError } from "@/schemas/error"
 import { errorSchema } from "@/schemas/error"
+import type { Spec } from "@/schemas/spec"
 import { specSchema } from "@/schemas/spec"
-import { toast } from "vue-sonner"
 
 export const useOpenAdminSpec = () => {
-	return useQuery({
+	return useQuery<Spec, ApiError>({
 		queryKey: ["openadmin-spec"],
 		queryFn: async () => {
-			return fetch("openadmin.json")
-				.catch((e) => errorSchema.parse(e))
-				.then((res) => specSchema.parse(res))
-				.catch((e) => toast.error(e))
+			const response = await fetch("openadmin.json")
+			const data = await response.json()
+
+			if (!response.ok) {
+				const error = errorSchema.parse(data)
+				throw error
+			}
+
+			return specSchema.parse(data)
 		},
 	})
 }
