f37460f1bff1a40e8a1268eff15dbe5a6640e145
Mykyta <mykytakasianenko@gmail.com>
2026-08-09 15:09:52 +0200

    feat: added biome

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-  <h1>Hello</h1>
+	<h1>Hello</h1>
 </template>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,6 @@
-import { createApp } from 'vue'
-import { VueQueryPlugin } from '@tanstack/vue-query'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue"
+import { VueQueryPlugin } from "@tanstack/vue-query"
+import "./style.css"
+import App from "./App.vue"
 
-createApp(App).use(VueQueryPlugin).mount('#app')
+createApp(App).use(VueQueryPlugin).mount("#app")
