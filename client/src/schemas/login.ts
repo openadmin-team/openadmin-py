a8e1d3087abdb7c354230c22e0cdc954dcb4be35
Mykyta <mykytakasianenko@gmail.com>
2026-08-13 22:51:49 +0200

    ref

--- client/src/schemas/login.ts
+++ client/src/schemas/login.ts
@@ -5,8 +5,8 @@
 import z from "zod"
 
 export const loginSchema = z.object({
-	username: z.string().min(1).max(100),
-	password: z.string().min(1).max(100),
+	username: z.string().min(1, "Username is required").max(100),
+	password: z.string().min(1, "Password is required").max(100),
 })
 
 export type Login = z.infer<typeof loginSchema>
