35b0a94c497bd3fdce228ac5f541ef12558905fa
Mykyta <mykytakasianenko@gmail.com>
2026-08-13 21:36:41 +0200

    ref

--- examples/admin/auth.py
+++ examples/admin/auth.py
@@ -11,9 +11,12 @@ auth = AdminAuth()
 
 @auth.login()
 def login(req: Request, login: LoginReq) -> None:
-    raise HTTPException(status.HTTP_401_UNAUTHORIZED, "UNAUTHORIZED login")
+    req.session.update({"token": "a"})
 
 
 @auth.authenticate()
 def authenticate(req: Request) -> None:
-    raise HTTPException(status.HTTP_401_UNAUTHORIZED, "UNAUTHORIZED authenticate")
+    token = req.session.get("token")
+
+    if not token == "a":
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, "Unauthorized")
