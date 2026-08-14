ff5a72589d0d164b696e6f02c93297c39e4d1756
Mykyta <mykytakasianenko@gmail.com>
2026-08-14 08:55:51 +0200

    ref

--- client/src/schemas/error.ts
+++ client/src/schemas/error.ts
@@ -8,4 +8,4 @@ export const errorSchema = z.object({
 	message: z.string(),
 })
 
-export type ApiError = z.infer<typeof errorSchema> & { status: string }
+export type ApiError = z.infer<typeof errorSchema>
