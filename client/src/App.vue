6fb62be2d031a150ade2bfe5051aabb6de255f80
Mykyta <mykytakasianenko@gmail.com>
2026-08-14 09:47:43 +0200

    ref

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,9 +3,14 @@ SPDX-FileCopyrightText: 2026 OpenAdmin
 
 SPDX-License-Identifier: AGPL-3.0-or-later
 -->
+<script setup lang="ts">
+import "vue-sonner/style.css"
+import { Toaster } from "@/components/ui/sonner"
+</script>
 
 <template>
 	<main>
 		<RouterView />
+		<Toaster />
 	</main>
 </template>
