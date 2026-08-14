65107911544eee72826ea0acc82d4829599c275b
Mykyta <mykytakasianenko@gmail.com>
2026-08-14 08:48:48 +0200

    ref

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,8 +4,8 @@
 
 import { createApp } from "vue"
 import { VueQueryPlugin } from "@tanstack/vue-query"
-import "./style.css"
-import App from "./App.vue"
-import { router } from "./router"
+import "@/style.css"
+import App from "@/App.vue"
+import { router } from "@/router"
 
 createApp(App).use(router).use(VueQueryPlugin).mount("#app")
