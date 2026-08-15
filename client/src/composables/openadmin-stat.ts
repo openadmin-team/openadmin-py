30d3698378d9c668a7b6d74a9087648de61e05f7
Mykyta <mykytakasianenko@gmail.com>
2026-08-15 20:36:00 +0200

    ref

--- client/src/composables/openadmin-stat.ts
+++ client/src/composables/openadmin-stat.ts
@@ -1,10 +1,25 @@
-import type { MaybeRefOrGetter } from "vue"
+// SPDX-FileCopyrightText: 2026 OpenAdmin
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import { computed, toValue, type MaybeRefOrGetter } from "vue"
 import { usePageSpec } from "./openadmin-spec"
 
-export const useStat = (params: {
-    sectionId: MaybeRefOrGetter<string>
-    pageId: MaybeRefOrGetter<string>
-    actionId: MaybeRefOrGetter<string | undefined>
+export const useStat = ({
+	sectionId,
+	pageId,
+	statId,
+}: {
+	sectionId: MaybeRefOrGetter<string>
+	pageId: MaybeRefOrGetter<string>
+	statId: MaybeRefOrGetter<string>
 }) => {
-    const {} = usePageSpec({sectionId, pageId})
+	const { page } = usePageSpec({ sectionId, pageId })
+	const stat = computed(() =>
+		page.value?.components.filter((c) => c.type === "stat" && c.id === toValue(statId)),
+	)
+
+	return {
+		stat,
+	}
 }
