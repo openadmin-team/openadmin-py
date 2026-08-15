eb7f0525f5a4f09e7c7f6ed0944b4e21325e4a42
Mykyta <mykytakasianenko@gmail.com>
2026-08-15 12:22:15 +0200

    ref

---
+++ client/src/layout/Dashboard.vue
@@ -0,0 +1,22 @@
+<!--
+SPDX-FileCopyrightText: 2026 OpenAdmin
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import AppSidebar from "@/components/dashboard/Sidebar.vue"
+
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+</script>
+
+<template>
+	<SidebarProvider>
+		<AppSidebar />
+		<SidebarInset>
+			<main class="flex-1 overflow-auto p-4 md:p-6">
+				<slot />
+			</main>
+		</SidebarInset>
+	</SidebarProvider>
+</template>
