0cf1ea52e840cf736fadc6bf39b83e1249f847da
Mykyta <mykytakasianenko@gmail.com>
2026-08-16 10:12:18 +0200

    ref

--- client/src/components/page/DataTable.vue
+++ client/src/components/page/DataTable.vue
@@ -1,5 +1,5 @@
-<script setup lang="ts">
-import type { ColumnDef } from '@tanstack/vue-table'
+<script setup lang="ts" generic="TData extends RowData">
+import type { ColumnDef, RowData } from '@tanstack/vue-table'
 import { FlexRender, useTable as useTanStackTable } from '@tanstack/vue-table'
 import {
   Table,
@@ -22,3 +22,36 @@ const table = useTanStackTable({
   get columns() { return props.columns },
 })
 </script>
+
+<template>
+  <div class="border rounded-md">
+    <Table>
+      <TableHeader>
+        <TableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
+          <TableHead v-for="header in headerGroup.headers" :key="header.id">
+            <FlexRender v-if="!header.isPlaceholder" :header="header" />
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <template v-if="table.getRowModel().rows?.length">
+          <TableRow
+            v-for="row in table.getRowModel().rows" :key="row.id"
+            :data-state="row.getIsSelected() && 'selected'"
+          >
+            <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
+              <FlexRender :cell="cell" />
+            </TableCell>
+          </TableRow>
+        </template>
+        <template v-else>
+          <TableRow>
+            <TableCell :colspan="columns.length" class="h-24 text-center">
+              No results.
+            </TableCell>
+          </TableRow>
+        </template>
+      </TableBody>
+    </Table>
+  </div>
+</template>
