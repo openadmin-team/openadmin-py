ab42043d3b7e1fd7d8e6858db6485aad6a25d2b4
Mykyta <mykytakasianenko@gmail.com>
2026-08-16 10:16:43 +0200

    ref

--- client/src/components/page/DataTable.vue
+++ client/src/components/page/DataTable.vue
@@ -1,57 +1,60 @@
 <script setup lang="ts" generic="TData extends RowData">
-import type { ColumnDef, RowData } from '@tanstack/vue-table'
-import { FlexRender, useTable as useTanStackTable } from '@tanstack/vue-table'
+import type { ColumnDef, RowData } from "@tanstack/vue-table"
+import { FlexRender, useTable as useTanStackTable } from "@tanstack/vue-table"
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table'
-import { features, type DataTableFeatures } from '@/lib/data-table'
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table"
+import { features, type DataTableFeatures } from "@/lib/data-table"
 
 const props = defineProps<{
-  columns: ColumnDef<DataTableFeatures, TData>[]
-  data: TData[]
+	columns: ColumnDef<DataTableFeatures, TData>[]
+	data: TData[]
 }>()
 
 const table = useTanStackTable({
-  features,
-  get data() { return props.data },
-  get columns() { return props.columns },
+	features,
+	get data() {
+		return props.data
+	},
+	get columns() {
+		return props.columns
+	},
 })
 </script>
 
 <template>
-  <div class="border rounded-md">
-    <Table>
-      <TableHeader>
-        <TableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
-          <TableHead v-for="header in headerGroup.headers" :key="header.id">
-            <FlexRender v-if="!header.isPlaceholder" :header="header" />
-          </TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <template v-if="table.getRowModel().rows?.length">
-          <TableRow
-            v-for="row in table.getRowModel().rows" :key="row.id"
-            :data-state="row.getIsSelected() && 'selected'"
-          >
-            <TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
-              <FlexRender :cell="cell" />
-            </TableCell>
-          </TableRow>
-        </template>
-        <template v-else>
-          <TableRow>
-            <TableCell :colspan="columns.length" class="h-24 text-center">
-              No results.
-            </TableCell>
-          </TableRow>
-        </template>
-      </TableBody>
-    </Table>
-  </div>
+	<div class="border rounded-md">
+		<Table>
+			<TableHeader>
+				<TableRow v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
+					<TableHead v-for="header in headerGroup.headers" :key="header.id">
+						<FlexRender v-if="!header.isPlaceholder" :header="header" />
+					</TableHead>
+				</TableRow>
+			</TableHeader>
+			<TableBody>
+				<template v-if="table.getRowModel().rows?.length">
+					<TableRow
+						v-for="row in table.getRowModel().rows"
+						:key="row.id"
+						:data-state="row.getIsSelected() && 'selected'"
+					>
+						<TableCell v-for="cell in row.getVisibleCells()" :key="cell.id">
+							<FlexRender :cell="cell" />
+						</TableCell>
+					</TableRow>
+				</template>
+				<template v-else>
+					<TableRow>
+						<TableCell :colspan="columns.length" class="h-24 text-center"> No results. </TableCell>
+					</TableRow>
+				</template>
+			</TableBody>
+		</Table>
+	</div>
 </template>
---
+++ client/src/components/page/Table.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useTable } from "@/composables/openadmin-table"
+import DataTable from "./DataTable.vue";
+
+const props = defineProps<{
+	sectionId: string
+	pageId: string
+	tableId: string
+}>()
+
+const { columns, rows } = useTable({
+	sectionId: props.sectionId,
+	pageId: props.pageId,
+	tableId: props.tableId,
+})
+</script>
+
+<template>
+     <div class="container py-10 mx-auto">
+    <DataTable :columns="columns" :data="rows" />
+  </div>
+</template>
