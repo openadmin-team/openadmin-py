27679380593ada3da6de41b84b69c0c208319843
Mykyta <mykytakasianenko@gmail.com>
2026-08-16 10:10:56 +0200

    ref

---
+++ client/src/components/page/DataTable.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { ColumnDef } from '@tanstack/vue-table'
+import { FlexRender, useTable as useTanStackTable } from '@tanstack/vue-table'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { features, type DataTableFeatures } from '@/lib/data-table'
+
+const props = defineProps<{
+  columns: ColumnDef<DataTableFeatures, TData>[]
+  data: TData[]
+}>()
+
+const table = useTanStackTable({
+  features,
+  get data() { return props.data },
+  get columns() { return props.columns },
+})
+</script>
