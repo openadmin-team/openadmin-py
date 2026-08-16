63237a0b4b02e28add4524c11757a90e8e6b095c
Mykyta <mykytakasianenko@gmail.com>
2026-08-16 11:57:51 +0200

    ref

--- client/src/components/page/DataTableDropDown.vue
+++ client/src/components/page/DataTableDropDown.vue
@@ -13,8 +13,6 @@ import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { useColor } from "@/composables/colors"
