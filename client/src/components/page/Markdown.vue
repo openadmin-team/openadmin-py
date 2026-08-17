45dae6d4ff00cc83435c2eb07382566e4ab45705
Mykyta <mykytakasianenko@gmail.com>
2026-08-17 21:22:49 +0200

    ref

---
+++ client/src/components/page/Markdown.vue
@@ -0,0 +1,11 @@
+<!--
+SPDX-FileCopyrightText: 2026 OpenAdmin
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<template>
+	<div class="typeset typeset-docs max-w-[37em]">
+		<!-- rendered markdown -->
+	</div>
+</template>
