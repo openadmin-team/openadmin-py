47a5a0c3cd09c5c10e625b5bd13a35ac4b3f17b7
Mykyta <mykytakasianenko@gmail.com>
2026-08-17 22:06:22 +0200

    ref

--- examples/admin/docs.py
+++ examples/admin/docs.py
@@ -7,37 +7,6 @@ from openadmin import fastapi, spec
 page = fastapi.AdminPage("Docs", icon="dock")
 
 
-@page.markdown("Company policy 1", icon="podcast", color="blue")
-def company_policy_1() -> spec.Markdown:
-    return """
-# Hello World
-
-This is new text
-
----
-
-- Cake
-- Limonade
-"""
-
-
-@page.markdown("Company policy 2")
-def company_policy_2() -> spec.Markdown:
-    return {
-        "content": """
-                    # Hello World
-
-                    This is new text
-
-                    ---
-
-                    - Cake
-                    - Limonade
-        """,
-        "color": "yellow",
-        "icon": "a-arrow-up",
-    }
-
 
 @page.markdown("Markdown Guide - Part 1", icon="heading", color="indigo")
 def markdown_guide_part_1() -> spec.Markdown:
