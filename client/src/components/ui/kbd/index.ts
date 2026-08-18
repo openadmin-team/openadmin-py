897eab22de3cb1ed5441d4b405fe48fd8593d38a
Mykyta <mykytakasianenko@gmail.com>
2026-08-18 20:05:56 +0200

    ref

--- client/src/components/ui/kbd/index.ts
+++ client/src/components/ui/kbd/index.ts
@@ -1,2 +1,6 @@
+// SPDX-FileCopyrightText: 2026 OpenAdmin
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 export { default as Kbd } from "./Kbd.vue"
 export { default as KbdGroup } from "./KbdGroup.vue"
