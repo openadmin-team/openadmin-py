f59363c0d3875bc7152823f46de05b6ffa6c4e03
Mykyta <mykytakasianenko@gmail.com>
2026-08-23 20:41:54 +0200

    feat: form component

---
+++ client/src/components/page/Form.vue
@@ -0,0 +1,25 @@
+<!--
+SPDX-FileCopyrightText: 2026 OpenAdmin
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import { useForm } from "@/composables/openadmin-form"
+
+const props = defineProps<{
+	sectionId: string
+	pageId: string
+	formId: string
+}>()
+
+const { form } = useForm({
+	sectionId: props.sectionId,
+	pageId: props.pageId,
+	formId: props.formId,
+})
+</script>
+
+<template>
+	<h1>Form ! {{ form }}</h1>
+</template>
--- client/src/views/FormView.vue
+++ client/src/views/FormView.vue
@@ -4,6 +4,18 @@ SPDX-FileCopyrightText: 2026 OpenAdmin
 SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 
+<script setup lang="ts">
+import { computed } from "vue"
+import { useRoute } from "vue-router"
+import Form from "@/components/page/Form.vue"
+
+const route = useRoute()
+
+const pageId = computed(() => route.params.pageId as string)
+const sectionId = computed(() => route.params.sectionId as string)
+const formId = computed(() => route.params.formId as string)
+</script>
+
 <template>
-	<h1>Home Form</h1>
+	<Form :section-id="sectionId" :page-id="pageId" :form-id="formId" />
 </template>
