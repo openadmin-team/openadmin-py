faaa3d1112ac0242a212547f8a11dbd69fc4d251
Mykyta <mykytakasianenko@gmail.com>
2026-08-24 21:49:59 +0200

    ref

--- examples/admin/form_test.py
+++ examples/admin/form_test.py
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-from datetime import date as date_type
 from datetime import datetime
 
 from openadmin import spec
@@ -17,11 +16,10 @@ def get_info(
     int: int,
     float: float,
     date: datetime,
-    day: date_type,
     bool: bool,
     list_of_strings: list[str],
     list_of_ints: list[int],
 ) -> spec.Form:
     return {
-        "toast": f"All good ! {string=} {int=} {float=} {date=} {day=} {bool=} {list_of_ints=} {list_of_strings=}"
+        "toast": f"All good ! {string=} {int=} {float=} {date=} {bool=} {list_of_ints=} {list_of_strings=}"
     }
