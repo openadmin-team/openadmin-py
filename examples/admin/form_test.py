c3a4c2e165959fdd2f1132d97442ed2b345b3407
Mykyta <mykytakasianenko@gmail.com>
2026-08-24 22:35:06 +0200

    ref

--- examples/admin/form_test.py
+++ examples/admin/form_test.py
@@ -6,9 +6,18 @@ from datetime import datetime
 from fastapi import UploadFile
 from openadmin import spec
 from openadmin.fastapi import AdminPage
+from typing import Literal
+from enum import Enum, StrEnum
 
 page = AdminPage("Test forms", icon="test-tube")
 
+class Methods(Enum):
+    GET = "GET"
+    POST = "POST"
+
+class MethodsStr(StrEnum):
+    GET = "GET"
+    POST = "POST"
 
 @page.form("Test all fields", method="post", icon="university", color="green")
 def get_info(
@@ -21,6 +30,9 @@ def get_info(
     list_of_ints: list[int],
     file: UploadFile,
     files: list[UploadFile],
+    literals: Literal['get', 'post'],
+    enum: Methods,
+    str_enum: MethodsStr,
 ) -> spec.Form:
     return {
         "toast": f"All good ! {string=} {int=} {float=} {date=} {bool=} {list_of_ints=} {list_of_strings=} {file.filename=} {len(files)=}"
