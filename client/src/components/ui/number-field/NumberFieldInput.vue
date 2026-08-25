50de2cf56ba02e24a67e3d3d11b98aee9071a640
Mykyta <mykytakasianenko@gmail.com>
2026-08-25 16:37:36 +0200

    ref

--- client/src/components/ui/number-field/NumberFieldInput.vue
+++ client/src/components/ui/number-field/NumberFieldInput.vue
@@ -18,7 +18,7 @@ const props = defineProps<{
 	<NumberFieldInput
 		data-slot="input"
 		:class="cn(
-			'placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-xs border bg-transparent px-3 py-1 text-center text-base shadow-xs transition-[color,box-shadow] outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+			'placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-center text-base shadow-xs transition-[color,box-shadow] outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
 			'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-3',
 			'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
 			props.class,
--- client/src/components/ui/tags-input/TagsInput.vue
+++ client/src/components/ui/tags-input/TagsInput.vue
@@ -25,7 +25,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
 		data-slot="tags-input"
 		v-bind="forwarded"
 		:class="cn(
-      'border-input flex min-h-9 w-full flex-wrap items-center gap-1.5 rounded-xs border bg-transparent px-2.5 py-1.5 text-sm shadow-xs transition-[color,box-shadow] outline-none',
+      'border-input flex min-h-9 w-full flex-wrap items-center gap-1.5 rounded-md border bg-transparent px-2.5 py-1.5 text-sm shadow-xs transition-[color,box-shadow] outline-none',
       'dark:bg-input/30',
       'focus-within:border-ring focus-within:ring-ring/50 focus-within:ring-3',
       'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
