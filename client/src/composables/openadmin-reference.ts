0743f5f8940a95bbae88b765a7e6511d6c902707
Mykyta <mykytakasianenko@gmail.com>
2026-09-01 20:41:00 +0200

    ref

---
+++ client/src/composables/openadmin-reference.ts
@@ -0,0 +1,6 @@
+import { useSpec } from "./openadmin-spec"
+
+export const openadminReference = ({ componentId }: {componentId: string}) => {
+    const { data } = useSpec()
+    // return compinentId, pageId and sectiondId
+}
