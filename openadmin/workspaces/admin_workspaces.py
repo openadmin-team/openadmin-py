5478eba3f9db87b3e284a4d147dffb6225eadc80
Mykyta <kasianenkomykyta@gmail.com>
2026-09-25 22:07:03 +0200

    feat: added workspaces

---
+++ openadmin/workspaces/admin_workspaces.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2026 OpenAdmin
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+
+class AdminWorkspaces:
+    def __init__(self) -> None:
+        pass
+
+    def workspace(self): ...
+
+    def workspaces(self): ...
+
+    def select_workspace(self): ...
