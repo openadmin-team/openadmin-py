0990a6c6ca66b0e09583ec49bc002846c8d82820
Mykyta <kasianenkomykyta@gmail.com>
2026-09-25 22:21:37 +0200

    ref

--- examples/admin/workspaces.py
+++ examples/admin/workspaces.py
@@ -1,13 +1,21 @@
-from openadmin.workspaces import AdminWorkspaces
+# SPDX-FileCopyrightText: 2026 OpenAdmin
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 from fastapi import Request
 
+from openadmin.workspaces import AdminWorkspaces
+
 workspeaces = AdminWorkspaces()
+
 
 @workspeaces.workspace()
 async def current_workspace(req: Request): ...
 
+
 @workspeaces.workspaces()
 async def get_workspaces_list(req: Request): ...
+
 
 @workspeaces.select_workspace()
 async def select_workspace(req: Request): ...
--- openadmin/workspaces/admin_workspaces.py
+++ openadmin/workspaces/admin_workspaces.py
@@ -2,16 +2,63 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-from typing import Callable, Awaitable, Any
+from collections.abc import Awaitable, Callable
+from typing import Any
+
 
 class AdminWorkspaces:
     def __init__(self) -> None:
-        self.workspace_func: Callable[[Any], None | Awaitable[None]]
-        self.workspaces_func: Callable[[Any], None | Awaitable[None]]
-        self.select_workspace_func: Callable[[str, Any], None | Awaitable[None]]
+        self.workspace_func: Callable[[Any], None | Awaitable[None]] = (
+            self.__create_default_workspace_func()
+        )
+        self.workspaces_func: Callable[[Any], None | Awaitable[None]] = (
+            self.__create_default_workspaces_func()
+        )
+        self.select_workspace_func: Callable[[str, Any], None | Awaitable[None]] = (
+            self.__create_default_select_workspace_func()
+        )
 
-    def workspace(self): ...
+    def workspace(self):
+        def _(
+            func: Callable[..., None | Awaitable[None]],
+        ) -> Callable:
 
-    def workspaces(self): ...
+            self.workspace_func = func
 
-    def select_workspace(self): ...
+            return func
+
+        return _
+
+    def workspaces(self):
+        def _(
+            func: Callable[..., None | Awaitable[None]],
+        ) -> Callable:
+
+            self.workspaces_func = func
+
+            return func
+
+        return _
+
+    def select_workspace(self):
+        def _(
+            func: Callable[..., None | Awaitable[None]],
+        ) -> Callable:
+
+            self.select_workspace_func = func
+
+            return func
+
+        return _
+
+    def __create_default_workspace_func(
+        self,
+    ) -> Callable[[Any], None | Awaitable[None]]: ...
+
+    def __create_default_workspaces_func(
+        self,
+    ) -> Callable[[Any], None | Awaitable[None]]: ...
+
+    def __create_default_select_workspace_func(
+        self,
+    ) -> Callable[[str, Any], None | Awaitable[None]]: ...
